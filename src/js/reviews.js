e9173262375086039fbf081e09a3bc24e05629bb
Nikita Bogatyrev <nbogatyrev@corp.kubra.com>
2025-03-31 17:13:06 -0400

    'new'

--- src/js/reviews.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector(".reviews-container-scroll");
-document.getElementById("scrollLeft").addEventListener("click", () => {
-  container.scrollBy({ left: -300, behavior: "smooth" });
-});
-document.getElementById("scrollRight").addEventListener("click", () => {
-  container.scrollBy({ left: 300, behavior: "smooth" });
-});
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import './js/backdrop.js'
-import './js/mobile-menu.js'
-import './js/reviews.js'
-
